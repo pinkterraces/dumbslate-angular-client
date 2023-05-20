f530f02f70121288b091f4240d2d3852b5708a15
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-05-20 13:43:39 +0200

    Adjusted all API endpoints

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,13 +9,13 @@ const apiUrl = 'https://dumbslate.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
+
+// User Endpoints
 export class UserRegistrationService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
   }
-
-  ///User Actions
   // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
@@ -23,51 +23,112 @@ export class UserRegistrationService {
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for the user login endpoint
+export class UserLoginService {
+  constructor(private http: HttpClient) {
+  }
   public userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.post(apiUrl + 'login', userDetails).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for the update user endpoint
-  updateUser(userDetails: any): Observable<any> {
+export class UpdateUserService {
+  constructor(private http: HttpClient) {
+  }
+  public updateUser(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.put(apiUrl + 'users', userDetails).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for the delete user endpoint
-  deleteUser(userDetails: any): Observable<any> {
+export class DeleteUserService {
+  constructor(private http: HttpClient) {
+  }
+  public deleteUser(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.delete(apiUrl + 'users', userDetails).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for the get single user endpoint
-  getUser(userDetails: any): Observable<any> {
+export class GetUserService {
+  constructor(private http: HttpClient) {
+  }
+  public getUser(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http.get(apiUrl + 'users', userDetails).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for the get all users endpoint
-  /*   public userGetAll(userDetails: any): Observable<any> {
-      console.log(userDetails);
-      return this.http.get(apiUrl + 'users', userDetails).pipe(
-        catchError(this.handleError)
-      );
-    } */
-
-
-  /// Movie Actions
-  // Making the api call for getting all movies endpoint
-  getAllMovies(): Observable<any> {
+/// Movie Endpoints
+export class GetAllMoviesService {
+  constructor(private http: HttpClient) {
+  }
+  public getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
     return this.http.get(apiUrl + 'movies', {
       headers: new HttpHeaders(
@@ -80,62 +141,143 @@ export class UserRegistrationService {
     );
   }
   // Non-typed response extraction
-  private extractResponseData(res: Response): any {
+  private extractResponseData(res: Response | Object): any {
     const body = res;
     return body || {};
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for getting single movie endpoint
-  getMovie(title: any): Observable<any> {
+export class GetMovieService {
+  constructor(private http: HttpClient) {
+  }
+  public getMovie(title: any): Observable<any> {
     console.log(title);
     return this.http.get(apiUrl + 'movies', title).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for getting director info endpoint
-  getGenre(genreName: any): Observable<any> {
-    console.log(genreName);
-    return this.http.get(apiUrl + 'movies' + 'genre', genreName).pipe(
+export class GetGenreService {
+  constructor(private http: HttpClient) {
+  }
+  public getGenre(genre: any): Observable<any> {
+    console.log(genre);
+    return this.http.get(apiUrl + 'movies/genre', genre).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for getting director info endpoint
-  getDirector(director: any): Observable<any> {
+export class GetDirectorService {
+  constructor(private http: HttpClient) {
+  }
+  public getDirector(director: any): Observable<any> {
     console.log(director);
-    return this.http.get(apiUrl + 'movies' + 'directors', director).pipe(
+    return this.http.get(apiUrl + 'movies/directors', director).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // UserMovie actions
-  // Making the api call for getting favourite movie for a user endpoint
-  getFavourites(movie: any, userDetails: any): Observable<any> {
-    console.log(movie);
-    return this.http.get(apiUrl + 'users' + userDetails + 'movies').pipe(
+/// UserMovie Endpoints
+export class GetUserFavouritesService {
+  constructor(private http: HttpClient) {
+  }
+  public getFavourites(movie: any, userName: any): Observable<any> {
+    console.log(userName, movie);
+    return this.http.get(apiUrl + 'users/' + userName + '/movies').pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for adding favourite movie to user endpoint
-  addFavourite(movie: any, userDetails: any): Observable<any> {
-    console.log(movie + ', ' + userDetails);
-    return this.http.post(apiUrl + 'users' + userDetails + 'movies', movie).pipe(
+export class AddFavouriteService {
+  constructor(private http: HttpClient) {
+  }
+  public addFavourite(movie: any, userName: any): Observable<any> {
+    console.log(userName, movie);
+    return this.http.post(apiUrl + 'users/' + userName + '/movies', movie).pipe(
       catchError(this.handleError)
     );
   }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
 
-  // Making the api call for deleting favourite movie to user endpoint
-  deleteFavourite(movie: any, userDetails: any): Observable<any> {
-    console.log(movie + ', ' + userDetails);
-    return this.http.delete(apiUrl + 'users' + userDetails + 'movies', movie).pipe(
+export class DeleteFavouriteService {
+  constructor(private http: HttpClient) {
+  }
+  public deleteFavourite(movie: any, userName: any): Observable<any> {
+    console.log(userName, movie);
+    return this.http.delete(apiUrl + 'users/' + userName + '/movies', movie).pipe(
       catchError(this.handleError)
     );
   }
-
-
-  //Error handling
   private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
       console.error('Some error occurred:', error.error.message);
